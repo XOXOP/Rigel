9b5310ec0ea96d9f8c2b6e30f8a6f2fdae37b17c
XOXOP <konh.n.lot.gmail.com>
2022-11-06 23:35:16 +0300

    1-е измениние  в файле

--- src/rigel/Main.java
+++ src/rigel/Main.java
@@ -1,51 +1,37 @@
 package rigel;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
 
 public class Main  {
-    static String[] strings;
-
+    static String[] stroka;
+    static String phrase;
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        String phrase = sc.nextLine();//"ввод выражения");
+        phrase = sc.nextLine();//"ввод выражения");
 
         if (phrase.startsWith("\"")) {
-             strings = phrase.split("\"");
+            stroka = phrase.split("\"");
+            if(stroka[1].length() <11) {
 
-            if(strings[1].length()<11){
-                String result =new String();
 
-                if (phrase.contains("+") && strings.length ==4) {
+                if (phrase.contains("+") || phrase.contains("-")) {
+                    Minus mn = new Minus();
+                    mn.cutPart();
 
-                        result = strings[1].concat(strings[3]);
-                        overSize(result);
 
-                } else if (phrase.contains("/")) {
+                } else if ((phrase.contains("/")) || (phrase.contains("*"))) {
 
                     Divider dv = new Divider();
-                    result =dv.findaNumber();
-                    overSize(result);
+                    dv.findaNumber();
 
-                } else if (phrase.contains("*")) {
-                    Multiply mt = new Multiply();
-                     result= mt.findaNumber();
-                    overSize(result);
-
-                } else if (phrase.contains("-")) {
-                    Minus mn = new Minus();
-                    result= mn.cutPart();
-                    overSize(result);
 
                 } else {
                     throw new IOException();
                 }
-
-            } else {
+            }else {
                 throw new IOException();
             }
         }else{
@@ -58,91 +44,82 @@ public class Main  {
         if(str1.length()<41){
             System.out.println("\"" + str1 +"\"");
         } else {
-            //str1= str1.substring(0,40);
-            System.out.println("\"" +str1.substring(0,40) + "...\"");
-
+            System.out.println("\"" +str1.substring(0,39) + "...\"");
         }
-
 
     }
 }
 
 class Divider {
-    public static String findaNumber() throws IOException {
+    public static void findaNumber() throws IOException {
+        String[] strin = Main.stroka;
+        if(strin.length <4) {
 
-        String[] strings = Main.strings;
-        strings[2] = strings[2].replace("/", " ");
-        strings[2] = strings[2].trim();
+            if (Main.phrase.contains("/")) {
+                strin[2] = strin[2].replace("/", " ");
 
-        int num = parseInt(strings[2]);
-        if(num >1 && num <11){
-            String resul = strings[1].substring(0, strings[1].length() / num);
 
-            return resul;
+            } else {
+                strin[2] = strin[2].replace("*", " ");
 
+            }
+            strin[2] = strin[2].trim();
+            int num = parseInt(strin[2]);
+            if (num > 0 && num < 11) {
+                if (Main.phrase.contains("/")) {
+                    System.out.println(strin[1].substring(0, strin[1].length() / num));
+
+                } else {
+                    String resul = strin[1];
+                    for (int i = 1; i < num; i++) {
+                        resul += strin[1];
+                    }
+                    Main.overSize(resul);
+                }
+
+            } else {
+                throw new IOException();
+            }
         }else {
             throw new IOException();
+
         }
-
-
-
 
     }
 }
 
 class Minus {
-    public static String cutPart() throws IOException {
+    public static void cutPart() throws IOException {
 
-        String[] strings = Main.strings;
-        if(strings[3].length()<11 && strings.length== 4){
-            String[] chart = strings[1].split(" ");
-            String[] sharp = strings[3].split(" ");
-
-            Collection c2 = new ArrayList<>();
-            for (String elem : chart) {
-                boolean add = c2.add(elem);
-            }
-            Collection e2 = new ArrayList<>();
-            for (String elem : sharp) {
-                boolean add = e2.add(elem);
-            }
-            c2.removeAll(e2);
-
-            String resul = " ";
-
-            for (Object str : c2) {
-                resul += str;
-            }
-
-            return resul;
+        String[] strin = Main.stroka;
+        boolean isBigger = ((strin.length !=4 )||( strin[3].length()>10));
+        if(isBigger) {
+            throw new IOException();
         }else {
-            throw new IOException();
-        }
+            String result = new String();
+            if (Main.phrase.contains("+")) {
+                result = strin[1].concat(strin[3]);
+                Main.overSize(result);
+            } else {
 
-    }
-}
-class Multiply{
+                String x = strin[3];
+                String str = strin[1];
 
-    public static String findaNumber() throws IOException {
+                String target = strin[1];
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.regionMatches(i, x, 0, x.length())) {
+                        target = str.substring(0, i);
+                        target += str.substring(i + x.length(), str.length());
+                    }
 
-        String[] strings = Main.strings;
-        strings[2] = strings[2].replace("*"," ");
-        strings[2] = strings[2].trim();
-
-        int num= parseInt(strings[2]);
-        if(num >1 && num <11) {
-
-            String resul = strings[1];
-            for (int i = 1; i < num; i++) {
-                resul += strings[1];
+                }
+                System.out.println("\"" + target + "\"");
             }
-
-            return resul;
-        }else{
-            throw new IOException();
         }
 
-    }
 
+    }
 }
+
+
 
